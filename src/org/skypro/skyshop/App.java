810b611b6e4d1fbeb9809319f195c4b7067d873a
Kanda0n <kononchenko1996@gmail.com>
2025-03-07 21:51:14 +0300

    Third part

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -5,16 +5,22 @@ import org.skypro.skyshop.product.DiscountedProduct;
 import org.skypro.skyshop.product.FixPriceProduct;
 import org.skypro.skyshop.product.Product;
 import org.skypro.skyshop.product.SimpleProduct;
+import org.skypro.skyshop.article.Article;
+import org.skypro.skyshop.search.SearchEngine;
+import org.skypro.skyshop.search.Searchable;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("HomeWork №13");
+        System.out.println("HomeWork №15");
         Product product1 = new SimpleProduct("Рис", 120);
         Product product2 = new SimpleProduct("Гречка", 60);
         Product product3 = new DiscountedProduct("Кола", 100, 20);
         Product product4 = new FixPriceProduct("Морковь");
         Product product5 = new SimpleProduct("Петрушка", 20);
         Product product6 = new DiscountedProduct("Чай", 150, 10);
+
+        Article article1 = new Article("Как готовить гречку", "Гречку нужно варить 20 минут.");
+        Article article2 = new Article("История Кока-Колы", "Кока-Кола была изобретена в 1886 году.");
 
         ProductBasket basket = new ProductBasket();
 
@@ -24,6 +30,29 @@ public class App {
         basket.addProduct(product4);
         basket.addProduct(product5);
         basket.addProduct(product6);
+
+        SearchEngine searchEngine = new SearchEngine(10);
+
+        searchEngine.add(product1);
+        searchEngine.add(product2);
+        searchEngine.add(product3);
+        searchEngine.add(product4);
+        searchEngine.add(product5);
+        searchEngine.add(product6);
+        searchEngine.add(article1);
+        searchEngine.add(article2);
+
+        System.out.println("Результаты поиска по запросу 'гречк':");
+        Searchable[] results1 = searchEngine.search("гречк");
+        printSearchResults(results1);
+
+        System.out.println("\nРезультаты поиска по запросу 'Кола':");
+        Searchable[] results2 = searchEngine.search("Кола");
+        printSearchResults(results2);
+
+        System.out.println("\nРезультаты поиска по запросу 'история':");
+        Searchable[] results3 = searchEngine.search("история");
+        printSearchResults(results3);
 
         System.out.println("Содержимое корзины:");
         basket.printBasket();
@@ -42,5 +71,13 @@ public class App {
         System.out.println("Общая стоимость пустой корзины: " + basket.getTotalCost());
 
         System.out.println("Есть ли в корзине Гречка? " + basket.containsProduct("Гречка"));
+    }
+
+    private static void printSearchResults(Searchable[] results) {
+        for (Searchable result : results) {
+            if (result != null) {
+                System.out.println(result.getStringRepresentation());
+            }
+        }
     }
 }
